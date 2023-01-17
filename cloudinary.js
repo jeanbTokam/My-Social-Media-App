2c5dab11de8df5fdf62b2c92c88c201560b79860
jbtokam <85589835+jeanbTokam@users.noreply.github.com>
2023-01-16 22:12:48 -0500

    Created Server

---
+++ cloudinary.js
@@ -0,0 +1,7 @@
+const cloudinary = require('cloudinary');
+cloudinary.config({ 
+  cloud_name: 'drgmbqrfe', 
+  api_key: '851883537592978', 
+  api_secret: 'mcQ9XNS4D39rV6Db8_zMk1SGzmw' 
+});
+  module.exports = {cloudinary}
---
+++ db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb+srv://tokam:tokam123@jb.gpgoi.mongodb.net/Social-media-app?retryWrites=true&w=majority' , {useUnifiedTopology: true , useNewUrlParser: true});
+
+const connection = mongoose.connection
+
+connection.on('connected' , ()=>{
+    console.log('Mongo db connection successfull')
+})
+
+connection.on('error' , ()=>{
+    console.log('Mongo db connection error')
+})
+
+module.exports = mongoose
